cbc3437eaa1bd771363c45a83f999f33ae2962ba
yogeshn8n <yogeshn8nwork@gmail.com>
2025-12-09 16:30:19 +0000

    06: dashboard sidebar

---
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
+import { BotIcon, StarIcon, VideoIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { DashboardUserButton } from "./dashboard-user-button";
+
+const firstSection = [
+    {
+        icon: VideoIcon,
+        label: "Meetings",
+        href: "/meetings",
+    },
+    {
+        icon: BotIcon,
+        label: "Agents",
+        href: "/agents",
+    },
+];
+
+const secondSection = [
+    {
+        icon: StarIcon,
+        label: "Upgrade",
+        href: "/upgrade",
+    },
+    
+];
+
+export const DashboardSidebar = () => {
+    const pathname = usePathname();
+    return (
+        <Sidebar>
+            <SidebarHeader className="text-sidebar-accent-foreground">
+                <Link href="/" className="flex items-center gap-2 px-2 pt-2">
+                    <Image src="/logo.svg" height={150} width={150} alt="AgentPal" />
+                </Link>
+            </SidebarHeader>
+            <div className="px-4 py-2">
+                <Separator className="opacity-70 text-[#0b0d0d]" />
+            </div>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {firstSection.map((item) => (
+                                <SidebarMenuItem key= {item.href}>
+                                    <SidebarMenuButton 
+                                    asChild
+                                    className={cn(
+                                        "h-10 hover:bg-linear-to-r/pklch border border-transparent hiver:border-[#5D6B68]/10 from-siderbar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                                        pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]"
+                                        )}
+                                        isActive={pathname === item.href}
+                                        >
+                                        <Link href={item.href}>
+                                        <item.icon className="size-5" />
+                                        <span className="text-sm font-medium tracking-tight">
+                                            {item.label}
+                                        </span>
+                                        </Link>
+                                    </SidebarMenuButton>                                    
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+                            <div className="px-4 py-2">
+                <Separator className="opacity-70 text-[#060707]" />
+            </div>
+                <SidebarGroup>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {secondSection.map((item) => (
+                                <SidebarMenuItem key= {item.href}>
+                                    <SidebarMenuButton 
+                                    asChild
+                                    className={cn(
+                                        "h-10 hover:bg-linear-to-r/pklch border border-transparent hiver:border-[#5D6B68]/10 from-siderbar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                                        pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]"
+                                        )}
+                                        isActive={pathname === item.href}
+                                        >
+                                        <Link href={item.href}>
+                                        <item.icon className="size-5" />
+                                        <span className="text-sm font-medium tracking-tight">
+                                            {item.label}
+                                        </span>
+                                        </Link>
+                                    </SidebarMenuButton>                                    
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter className="text-white">
+                <DashboardUserButton />
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
+
+
+
+
