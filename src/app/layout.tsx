aeb01d89a997b4a5dbfecad7c58a7292ae3ac7b3
yogeshn8n <yogeshn8nwork@gmail.com>
2026-03-27 13:51:13 +0000

    08: trpc setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { TRPCReactProvider } from "@/trpc/client";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,9 +26,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <TRPCReactProvider>
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}> {children}
       </body>
     </html>
+    </TRPCReactProvider>
   );
 }
